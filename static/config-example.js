d5111128f79e393e7c11996c3869bd9d2b96d36e
recidvst <cdsnowden@gmail.com>
2018-10-11 18:50:22 +0100

    add example config file

---
+++ static/config-example.js
@@ -0,0 +1,15 @@
+// config.js
+
+// app requires a config.js file to connect to the API
+// APIURL is the API endpoint
+// APITOKEN is the JWT auth token provided when logging a user in (does not expire)
+
+export function APIURL() {
+    return 'url';
+};
+
+export function APITOKEN() {
+    return 'token';
+};
+
+
