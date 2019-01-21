95672dfdfc32b9861505662297c86b26a7624657
recidvst <cdsnowden@gmail.com>
2019-01-21 23:09:30 +0000

    re-track config

---
+++ static/config.js
@@ -0,0 +1,11 @@
+
+// config objects
+export function APIURL() {
+    return 'https://moby-dick-lorem-ipsum-api.chris-snowden.me';
+};
+
+export function APITOKEN() {
+    return 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImNkc25vd2RlbkBnbWFpbC5jb20iLCJfaWQiOiI1Yjk2YmFlNWQ5ODAyNzEyZDRjYmJmYzAiLCJpYXQiOjE1MzY4NjkyNjd9.dIWtL1oBPEkYn3htWzY3U9EMjtewKb2yvmhB8PS3x_g';
+};
+
+
