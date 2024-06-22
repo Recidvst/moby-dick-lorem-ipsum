d57344339319bd3582de416a147f96b654c691d0
Chris Snowden <cdsnowden@gmail.com>
2024-06-22 21:43:53 +0100

    fix two minor eslint warnings

--- components/icons/copyIcon.vue
+++ components/icons/copyIcon.vue
@@ -53,7 +53,7 @@ export default {
               snippetID,
               bookType,
               contentType: snippetType,
-            }
+            },
           });
           target.classList.add('clicked');
           setTimeout(() => {
